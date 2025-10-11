5cb9f4f78c25c836aac6330c9872b7c8e96b1b14
Eric Mencarini <ericmencarini@outlook.com>
2025-10-11 16:09:58 -0300

    first_assignment_task

---
+++ 01.Dimensional_Data_Modeling/01_assignment_task.sql
@@ -0,0 +1,20 @@
+CREATE TYPE films AS
+(
+		films TEXT, 
+		votes INTEGER,
+		rating REAL,		
+        filmid INTEGER
+)
+
+CREATE TYPE quality_class AS 
+    ENUM('star','good','average','bad')
+
+CREATE TABLE actors (
+    actor_id INTEGER,
+    actor_name TEXT,
+    films films[],
+    quality_class quality_class,
+    is_active BOOLEAN,
+    year_film INTEGER,    
+    PRIMARY KEY(actor_id)
+)
