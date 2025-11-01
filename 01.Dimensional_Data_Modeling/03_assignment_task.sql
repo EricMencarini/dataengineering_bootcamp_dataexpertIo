8b3bdb28e2d6fd686c079cc3e1eebe310c3ffd1d
Eric Mencarini <ericmencarini@outlook.com>
2025-11-01 09:58:59 -0300

    03_assignment_task

--- 01.Dimensional_Data_Modeling/03_assignment_task.sql
+++ 01.Dimensional_Data_Modeling/03_assignment_task.sql
@@ -3,3 +3,14 @@
     - Implements type 2 dimension modeling (i.e., includes `start_date` and `end_date` fields).
     - Tracks `quality_class` and `is_active` status for each actor in the `actors` table.
 */
+CREATE TABLE actors_history_scd (
+    actorid TEXT,
+    actorname TEXT,
+    films films[],
+    quality_class quality_class,
+    is_active BOOLEAN,
+    current_year INTEGER,
+    start_year DATE,
+    end_year DATE,      
+    PRIMARY KEY(actorid, start_year)
+)
