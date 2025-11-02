1851ff90cbff346aff0a7558191c0d3d4869faf2
Eric Mencarini <ericmencarini@outlook.com>
2025-11-02 15:39:44 -0300

    add_07_assignment_task

--- 02.fact_data_modeling/07_assignment_task.sql
+++ 02.fact_data_modeling/07_assignment_task.sql
@@ -5,3 +5,11 @@
 - hit_array - think COUNT(1)
 - unique_visitors array -  think COUNT(DISTINCT user_id)
 */
+
+CREATE TABLE host_activity_reduced (
+    host TEXT,
+    month DATE,
+    hit_array REAL[] 
+    unique_visitors REAL[],
+    PRIMARY KEY(host, month)
+);
