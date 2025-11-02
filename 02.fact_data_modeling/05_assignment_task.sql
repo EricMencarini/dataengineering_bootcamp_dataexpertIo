5fc3e2f3043928901d064d40350c5ef1e632ded4
Eric Mencarini <ericmencarini@outlook.com>
2025-11-02 11:57:32 -0300

    add_05_assignment_task

--- 02.fact_data_modeling/05_assignment_task.sql
+++ 02.fact_data_modeling/05_assignment_task.sql
@@ -2,3 +2,9 @@
 5- A DDL for `hosts_cumulated` table 
   - a `host_activity_datelist` which logs to see which dates each host is experiencing any activity
 */
+CREATE TABLE hosts_cumulated AS (
+    host TEXT,
+    host_activity_datelist DATE[],
+    date DATE,
+  PRIMARY KEY(host, date)
+);
